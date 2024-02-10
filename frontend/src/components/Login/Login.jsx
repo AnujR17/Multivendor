a0e92d2b898fbeec14c4149a9eafb25dd57ce226
pratik2894 <mohitepratik155@gmail.com>
2024-02-10 22:47:48 +0530

    login complete

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,15 +1,82 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div className="min-h-screen bg-zinc-800 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-yellow-400">
-            Login to your account
-            </h2>
+<div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-md">
+    <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      Login to your account
+    </h2>
+  </div>
+  <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+      <form class="space-y-6">
+        <div>
+          <label for="email" class="block text-sm font-medium text-gray-700">
+            Email address
+          </label>
+          <div class="mt-1">
+            <input
+              type="email"
+              name="email"
+              autoComplete="email"
+              required
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
         </div>
+        <div>
+          <label for="password" class="block text-sm font-medium text-gray-700">
+            Password
+          </label>
+          <div class="mt-1 relative">
+            <input
+              type="password"
+              name="password"
+              autoComplete="current-password"
+              required
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
+        </div>
+        <div class="flex justify-between">
+          <div class="flex items-center">
+            <input
+              type="checkbox"
+              name="remember-me"
+              id="remember-me"
+              class="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+            />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+              Remember me
+            </label>
+          </div>
+          <div class="text-sm">
+            <a href=".forgot-password" class="font-medium text-blue-600 hover:text-blue-500">
+              Forgot your password?
+            </a>
+          </div>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+          >
+            Submit
+          </button>
+        </div>
+        <div class="flex w-full">
+          <h4>Not have any account?</h4>
+          <a href="/sign-up" class="text-blue-600 pl-2">
+            Sign Up
+          </a>
+        </div>
+      </form>
     </div>
-  )
-}
+  </div>
+</div>
 
-export default Login
+  );
+};
+
+export default Login;
